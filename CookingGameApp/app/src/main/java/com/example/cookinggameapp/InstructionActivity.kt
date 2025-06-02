ef93a6166f6390630deed6c03067cb9589692a65
Yar0s1av-O <yaroslav.oleinychenko@student.nhlstenden.com>
2025-06-02 13:13:50 +0200

    Update InstructionActivity.kt

--- CookingGameApp/app/src/main/java/com/example/cookinggameapp/InstructionActivity.kt
+++ CookingGameApp/app/src/main/java/com/example/cookinggameapp/InstructionActivity.kt
@@ -13,7 +13,7 @@ class InstructionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_instruction)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.boardBackground)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.backButton)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
