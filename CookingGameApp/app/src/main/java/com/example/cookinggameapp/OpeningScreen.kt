7c90f6b2aafc6db859e021929d98b4b93c34830a
bochengpeng <146732012+bochengpeng@users.noreply.github.com>
2025-06-10 22:22:55 +0200

    SCRUM-103 Bocheng add dropping action

--- CookingGameApp/app/src/main/java/com/example/cookinggameapp/OpeningScreen.kt
+++ CookingGameApp/app/src/main/java/com/example/cookinggameapp/OpeningScreen.kt
@@ -2,6 +2,8 @@ package com.example.cookinggameapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.delay
@@ -13,8 +15,13 @@ class OpeningScreen : BaseActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_animation)
 
+        // Start the egg drop animation
+        val egg = findViewById<ImageView>(R.id.fried_egg)
+        val dropAnim = AnimationUtils.loadAnimation(this, R.anim.animation_action)
+        egg.startAnimation(dropAnim)
+
         lifecycleScope.launch {
-            delay(5000) // 5000 milliseconds = 5 seconds
+            delay(5000) // 5 seconds
             startActivity(Intent(this@OpeningScreen, MainActivity::class.java))
             finish()
         }
