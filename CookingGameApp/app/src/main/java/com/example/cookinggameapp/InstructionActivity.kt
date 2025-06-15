59640fff7c35f45564f095f4ffac3fe3a563838f
Daryl Genove <daryl.genove@student.nhlstenden.com>
2025-06-15 20:59:38 +0200

    Reused User interface and made it responsive but not fully

--- CookingGameApp/app/src/main/java/com/example/cookinggameapp/InstructionActivity.kt
+++ CookingGameApp/app/src/main/java/com/example/cookinggameapp/InstructionActivity.kt
@@ -3,20 +3,21 @@ package com.example.cookinggameapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
-import androidx.activity.enableEdgeToEdge
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class InstructionActivity : BaseActivity() {
+class InstructionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_instruction)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.backButton)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        findViewById<TextView>(R.id.instructionTitle).text = "How to Play?"
+        findViewById<TextView>(R.id.instructionSteps).text = """
+            1. Host player creates a room
+            2. Other players enter the game code
+            3. Host player starts the game
+            4. Play together and have fun!
+        """.trimIndent()
 
         findViewById<ImageButton>(R.id.backButton).setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
--- CookingGameApp/app/src/main/java/com/example/cookinggameapp/RecipeGameActivity.kt
+++ CookingGameApp/app/src/main/java/com/example/cookinggameapp/RecipeGameActivity.kt
@@ -2,6 +2,7 @@ package com.example.cookinggameapp
 
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class RecipeGameActivity : AppCompatActivity() {
@@ -9,9 +10,15 @@ class RecipeGameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipe_game)
 
-        // Handle the back button click to close the activity
-        val backButton = findViewById<ImageButton>(R.id.backButton)
-        backButton.setOnClickListener {
+        findViewById<TextView>(R.id.instructionTitle).text = "Boiled Chicken"
+        findViewById<TextView>(R.id.instructionSteps).text = """
+            1. Chop the chicken
+            2. Put it in the pot
+            3. Boil it
+            4. Put a little salt
+        """.trimIndent()
+
+        findViewById<ImageButton>(R.id.backButton).setOnClickListener {
             finish()
         }
     }
